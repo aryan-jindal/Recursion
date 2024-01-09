82956ce920822dc8988cbd8d3d03775d26d0bdb2
aryan-jindal <aryan.jindal.d@gmail.com>
2024-01-09 19:46:44 +0100

    third1

--- src/com/algorithem/BagTest.java
+++ src/com/algorithem/BagTest.java
@@ -5,26 +5,16 @@ import java.io.IOException;
 public class BagTest {
 
 	public static void main(String[] args) throws IOException {
-		/*
-		 * Bag bag = new Bag();
-		 * 
-		 * bag.pack();
-		 * 
-		 * System.out.println(bag.getItems());
-		 * 
-		 * bag.unPack();
-		 * 
-		 * System.out.println(bag.getItems());
-		 */
 		
-		CallManager call = new CallManager();
+		  Bag bag = new Bag();
+		  
+		  bag.pack();
+		  
+		  System.out.println(bag.getItems());
+		  
+		  bag.unPack();
+		  
+		  System.out.println(bag.getItems());
 		
-		call.addCaller();
-		
-		System.out.println(call.getCaller());
-		
-		call.removeCaller();
-		
-		System.out.println(call.getCaller());
 	}
 }
